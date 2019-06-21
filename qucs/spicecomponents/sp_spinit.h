8bb6d92363c2c12c7c189d3fae791bc58040d349
spueblas <spueblas@google.com>
2019-06-21 11:28:55 -0400

    Adding new sp_spinit spice component. Not functional yet.

---
+++ qucs/spicecomponents/sp_spinit.h
@@ -0,0 +1,32 @@
+/***************************************************************************
+                                 sp_spinit.h
+                                -------------
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef SP_SPINIT_H
+#define SP_SPINIT_H
+
+#include "components/component.h"
+
+
+class SpiceSpinit : public Component  {
+public:
+  SpiceSpinit();
+  ~SpiceSpinit();
+  Component* newOne();
+  static Element* info(QString&, char* &, bool getNewOne=false);
+
+protected:
+  QString spice_netlist(bool isXyce = false);
+};
+
+#endif
