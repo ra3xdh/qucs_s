8129b407afe57f7d70ef46f0bf2f0c83788b7481
dsm <muhammet.demir@fennet.com.tr>
2023-02-28 14:32:31 +0300

    add msvc build support.
    - added msvc compiler flags.
    - fixed wrong file encoding.
    - change VLA to fixed or dynamic array for msvc.
    - succesfully build with qt6.5.0 and vs2019 on win10 native.

--- qucs-filter/qf_poly.cpp
+++ qucs-filter/qf_poly.cpp
@@ -46,7 +46,7 @@ qf_poly::qf_poly (qf_double_t a, qf_double_t b, qf_double_t c, unsigned deg) {
 
 #ifdef _QF_POLY_DEBUG
   std::cout << "qf_poly (ax^2+bx+c), a = " << a << ", b = " << b
-    << ", c = " << c << ", d° = " << deg << "\n";
+    << ", c = " << c << ", dÂ° = " << deg << "\n";
 #endif
 
   // Pathological cases 
@@ -67,7 +67,7 @@ qf_poly::qf_poly (qf_double_t a, qf_double_t b, qf_double_t c, unsigned deg) {
     d = 0;
     p = new qf_double_t[1];
     p[0] = a;
-    rts = NULL;			// no root (or an infinite # of them)
+    rts = NULL;			// no root (or an infinite #Â of them)
     krts = a;
     rep = BOTH;
     break;
@@ -84,7 +84,7 @@ qf_poly::qf_poly (qf_double_t a, qf_double_t b, qf_double_t c, unsigned deg) {
     rep = BOTH;
     break;
   default:
-    // Polynom of d° 2 (aX^2 + bX + c)
+    // Polynom of dÂ° 2 (aX^2 + bX + c)
     if (deg > 2)
       std::cout << "Warning: qf_poly called with deg > 2.\n";
     d = 2;
@@ -214,7 +214,7 @@ qf_double_t & qf_poly::operator [] (int i) {
   return rts[i];
 }
 
-// Returns d° (order) of polynom
+// Returns dÂ° (order) of polynom
 unsigned qf_poly::deg () {
   return d;
 }
