4ab7bc4a5c45b6e946fef3a2975c73abff74413c
Radvall <sergey.righov@gmail.com>
2023-04-20 18:45:12 +0300

    fixed load capacitor node name

--- qucs/components/d_flipflop.cpp
+++ qucs/components/d_flipflop.cpp
@@ -142,7 +142,7 @@ QString D_FlipFlop::spice_netlist(bool isXyce)
     s += " " + tmp_model + "\n";
     s += QString(".model %1 d_dff(clk_delay=%2 set_delay=%2 reset_delay=%2 rise_delay=%2 fall_delay=%2)\n")
             .arg(tmp_model).arg(td);
-    s += QString("C%1 NQ_%1 0 1N \n").arg(Name); // capacitor load for unused NQ pin
+    s += QString("C%1 QB_%1 0 1e-9 \n").arg(Name); // capacitor load for unused QB pin
 
     return s;
 }
