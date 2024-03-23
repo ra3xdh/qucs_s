8ae85e41608ee92de3fc6597875cb935c298725a
Andrey Kalmykov <wawuwo@ya.ru>
2024-03-23 19:23:09 +0100

    Move temporary files to StandardPaths::CacheLocation
    With this commit the root path for temporary files produced
    by Qucs-S is set to StandardPaths::CacheLocation which is
    ~/.cache/qucs-s on Linux.
    
    This path is set automatically and can't be changed.

--- qucs/main.h
+++ qucs/main.h
@@ -78,6 +78,13 @@ struct tQucsSettings {
   unsigned int NodeWiring;
   QDir QucsWorkDir;
   QDir QucsHomeDir;
+
+  // This is the dir where all temporary or intermediate data should be stored.
+  // Consider a data "temporary" if its used only once or it makes sense only
+  // through out a single app run or a shorter period of time.
+  // Don't make any assumptions about the lifetime of contents in this dir,
+  // think that everything placed in here is deleted when app is terminated.
+  QDir tempFilesDir;
   QDir projsDir; // current user projects subdirectory
   QDir AdmsXmlBinDir;  // dir of admsXml executable
   QDir AscoBinDir;     // dir of asco executable
