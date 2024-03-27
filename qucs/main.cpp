6411ada793e9e586b9da3f7876822014abef2a5a
Andrey Kalmykov <wawuwo@ya.ru>
2024-03-27 19:29:57 +0300

    Fix simulator output path setting persistence problem
    With this commit user-defined path has effect not only until app
    termination, but after the app starts again too, i.e. the setting
    is persisted and the bug which led to resetting this path to
    default on app start is fixed.

--- qucs/main.cpp
+++ qucs/main.cpp
@@ -157,7 +157,11 @@ bool loadSettings()
     // All usages of this path look like something involving a temporary data.
     // This should be replaced with generic temp dir, but for now let's just
     // place it under generic temp dir.
-    QucsSettings.S4Qworkdir = QDir::toNativeSeparators(QStandardPaths::writableLocation(QStandardPaths::CacheLocation) + "/spice4qucs");
+    if(settings.contains("S4Q_workdir")) {
+      QucsSettings.S4Qworkdir = settings.value("S4Q_workdir").toString();
+    } else {
+      QucsSettings.S4Qworkdir = QDir::toNativeSeparators(QStandardPaths::writableLocation(QStandardPaths::CacheLocation) + "/spice4qucs");
+    }
     if(settings.contains("SimParameters")) QucsSettings.SimParameters = settings.value("SimParameters").toString();
     else QucsSettings.SimParameters = "";
     if(settings.contains("OctaveExecutable")) {
