66ba88147c08054a57af8be86e684c9c2b3f21bb
Andrey Kalmykov <wawuwo@ya.ru>
2024-06-23 11:50:18 +0200

    Make AC sensitivity simulation inherit from SimulationComponent

--- qucs/spicecomponents/sp_sens_ac.cpp
+++ qucs/spicecomponents/sp_sens_ac.cpp
@@ -15,7 +15,6 @@
  *                                                                         *
  ***************************************************************************/
 #include "sp_sens_ac.h"
-#include "main.h"
 #include "extsimkernels/spicecompat.h"
 
 
@@ -24,20 +23,7 @@ SpiceSENS_AC::SpiceSENS_AC()
   isSimulation = true;
   Description = QObject::tr("AC sensitivity simulation");
   Simulator = spicecompat::simNgspice | spicecompat::simSpiceOpus;
-
-  QString  s = Description;
-  int a = s.indexOf(" ");
-  if (a != -1) s[a] = '\n';
-
-  Texts.append(new Text(0, 0, s.left(a), Qt::darkRed, QucsSettings.largeFontSize));
-  if (a != -1)
-    Texts.append(new Text(0, 0, s.mid(a+1), Qt::darkRed, QucsSettings.largeFontSize));
-
-  x1 = -10; y1 = -9;
-  x2 = x1+104; y2 = y1+59;
-
-  tx = 0;
-  ty = y2+1;
+  initSymbol(Description);
   Model = ".SENS_AC";
   Name  = "SENS";
   SpiceModel = ".SENS";
