ra3xdh/qucs_s

commit 4e9e9680cb9819bdadb42bcd3e06396967d7a531

Add "QPainter" version of Node::paint

qucs/node.cpp
@@ -59,6 +59,39 @@ void Node::paint(ViewPainter *p)
   }
 }
 
+void Node::paint(QPainter* painter) const {
+  painter->save();
+
+  switch(Connections.count()) {
+    case 1:
+      if (Label) {
+        painter->fillRect(cx-2, cy-2, 4, 4, Qt::darkBlue); // open but labeled
+      } else {
+        painter->setPen(QPen(Qt::red,1));  // node is open
+        painter->drawEllipse(cx-4, cy-4, 8, 8);
+      }
+      painter->restore();
+      return;
+
+    case 2:
+      if (Connections.getFirst()->Type == isWire) {
+        if (Connections.getLast()->Type == isWire) {
+          painter->restore();
+          return;
+        }
+        painter->fillRect(cx-2, cy-2, 4, 4, Qt::darkBlue);
+      }
+      break;
+
+    default:
+        painter->setBrush(Qt::darkBlue);  // more than 2 connections
+	      painter->setPen(QPen(Qt::darkBlue,1));
+	      painter->drawEllipse(cx-3, cy-3, 6, 6);
+	      painter->setBrush(Qt::NoBrush);
+  }
+  painter->restore();
+}
+
 // ----------------------------------------------------------------
 bool Node::getSelected(int x_, int y_)
 {

qucs/node.h
@@ -31,6 +31,7 @@ public:
  ~Node();
 
   void  paint(ViewPainter*);
+  void  paint(QPainter* painter) const;
   bool  getSelected(int, int);
   void  setName(const QString&, const QString&, int x_=0, int y_=0);
 

commit 5d11d94003831c6b6d399645629db2bf3a0db2ca

Draw component port as filled square when it has only one connection
This is a fix of regression introduced in 4e9e9680, the problem
is described in ra3xdh/qucs_s#818

qucs/node.cpp
@@ -50,13 +50,11 @@ void Node::paint(QPainter* painter) const {
       return;
 
     case 2:
-      if (Connections.getFirst()->Type == isWire) {
-        if (Connections.getLast()->Type == isWire) {
+      if (Connections.getFirst()->Type == isWire && Connections.getLast()->Type == isWire) {
           painter->restore();
           return;
-        }
-        painter->fillRect(cx-2, cy-2, 4, 4, Qt::darkBlue);
       }
+      painter->fillRect(cx-2, cy-2, 4, 4, Qt::darkBlue);
       break;
 
     default: