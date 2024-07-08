db75f6f25366a3532c592b904533c692cb3adf1d
Muhammet Şükrü Demir <41967334+dsm@users.noreply.github.com>
2024-07-09 00:38:53 +0300

    fix Qt5 build.

--- qucs/schematic.cpp
+++ qucs/schematic.cpp
@@ -2630,8 +2630,11 @@ void Schematic::contentsNativeGestureZoomEvent( QNativeGestureEvent* Event) {
   App->editText->setHidden(true); // disable edit of component property
 
   const auto factor = 1.0 + Event->value();
-
+#if (QT_VERSION >= QT_VERSION_CHECK(6, 0, 0))
   const auto pointer = mapFromGlobal(Event->globalPosition().toPoint());
+#else
+  const auto pointer = mapFromGlobal(Event->globalPos());
+#endif
   zoomAroundPoint(factor,pointer);
 }
 
