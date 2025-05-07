4217802a4607de1f1f7bc291a7c79e084483cddb
Andrey Kalmykov <wawuwo@ya.ru>
2025-05-07 19:32:08 +0200

    Add 'type' and 'simulator' fields to Property

--- qucs/components/property.h
+++ qucs/components/property.h
@@ -3,25 +3,34 @@
 
 #include <QRect>
 #include <QString>
+#include "../extsimkernels/spicecompat.h"
 
 class QPainter;
 
 struct Property {
+  enum class Type { File, Equation, Value };
+
   Property() = delete;
   Property(const QString& propertyName,
            const QString& propertyValue,
            bool isVisibleByDefault = false,
-           const QString& description = "")
+           const QString& description = "",
+           Type propertyType = Type::Value,
+           spicecompat::Simulator supportedSims = spicecompat::Simulator::simAll)
       : Name(propertyName)
       , Value(propertyValue)
       , display(isVisibleByDefault)
       , Description(description)
+      , type(propertyType)
+      , simulators(supportedSims)
       {};
 
   QString Name;
   QString Value;
   bool    display;   // show on schematic or not ?
   QString Description;
+  Type type;
+  spicecompat::Simulator simulators;
   QRect boundingRect() const { return br; };
   void paint(int x, int y, QPainter* p);
 private:
