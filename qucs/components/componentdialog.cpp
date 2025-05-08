ec413c3281727e32af0956ee6008b6390ebcec69
Andrey Kalmykov <wawuwo@ya.ru>
2025-05-08 13:38:46 +0200

    Hide simulator incompatible properties from component dialog

--- qucs/components/componentdialog.cpp
+++ qucs/components/componentdialog.cpp
@@ -635,6 +635,10 @@ void ComponentDialog::updatePropertyTable(const Component* updateComponent)
     int row = 0;
     for (const Property* property : updateComponent->Props)
     {
+      if ((property->simulators & QucsSettings.DefaultSimulator) != QucsSettings.DefaultSimulator) {
+        continue;
+      }
+
       if (hasSweep && sweepProperties.contains(property->Name))
         continue;
 
@@ -802,6 +806,10 @@ void ComponentDialog::slotApplyButton()
     row = 0;
     for (Property* property : component->Props)
     {
+      if ((property->simulators & QucsSettings.DefaultSimulator) != QucsSettings.DefaultSimulator) {
+        continue;
+      }
+
       // Ignore sweep parameters here.
       if (hasSweep && sweepParamWidget.contains(property->Name))
         continue;
