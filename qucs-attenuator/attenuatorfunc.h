e856852da0db240f62b858d0dc6e5ef813ec84f1
Andrey Kalmykov <wawuwo@ya.ru>
2025-06-23 10:35:49 +0200

    Format all files in ./qucs-attenuator using WebKit style

--- qucs-attenuator/attenuatorfunc.h
+++ qucs-attenuator/attenuatorfunc.h
@@ -29,41 +29,39 @@
 
 #include <cmath>
 
-struct tagATT
-{
-  int Topology;
-  double Zin;
-  double Zout;
-  double Attenuation;
-  double MinimumATT;
-  double R1;
-  double R2;
-  double R3;
-  double R4;
-  double L; // Length of the quarter-wavelength inverter. QW series/shunt attenuator only
-  double PR1;//Power dissipated by R1 [W]
-  double PR2;//Power dissipated by R2 [W]
-  double PR3;//Power dissipated by R3 [W]
-  double PR4;//Power dissipated by R4 [W]
-  double Pin;//Input power [W]
-  bool minR; //The reflection attenuator can be designed using two different resistor values. The first one is such
-             //as R < Z0 whereas the other is such as R > Z0. This field is just a flag to indicate what solution
-             //qucsattenuator should use.
-  double freq;//Central frequency for quarter-wavelength attenuators
-  bool useLumped;//Used only in the quarter wave attenuators. It indicates that the program must replace the
-                   //qw line by its lumped element equivalent (CLC)
+struct tagATT {
+    int Topology;
+    double Zin;
+    double Zout;
+    double Attenuation;
+    double MinimumATT;
+    double R1;
+    double R2;
+    double R3;
+    double R4;
+    double L; // Length of the quarter-wavelength inverter. QW series/shunt attenuator only
+    double PR1; // Power dissipated by R1 [W]
+    double PR2; // Power dissipated by R2 [W]
+    double PR3; // Power dissipated by R3 [W]
+    double PR4; // Power dissipated by R4 [W]
+    double Pin; // Input power [W]
+    bool minR; // The reflection attenuator can be designed using two different resistor values. The first one is such
+               // as R < Z0 whereas the other is such as R > Z0. This field is just a flag to indicate what solution
+               // qucsattenuator should use.
+    double freq; // Central frequency for quarter-wavelength attenuators
+    bool useLumped; // Used only in the quarter wave attenuators. It indicates that the program must replace the
+                    // qw line by its lumped element equivalent (CLC)
 };
 
 class QString;
 
-class QUCS_Att
-{
- public:
-  QUCS_Att();
-  ~QUCS_Att();
+class QUCS_Att {
+public:
+    QUCS_Att();
+    ~QUCS_Att();
 
-  int Calc(tagATT*);
-  static QString* createSchematic(tagATT*, bool);
+    int Calc(tagATT*);
+    static QString* createSchematic(tagATT*, bool);
 };
 
 QString num2str(double);
