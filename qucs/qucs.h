50991888e4c956c93d3ca85566e48e12b54af4c4
Torleif Skår <16509259+tskaar@users.noreply.github.com>
2025-07-28 22:30:45 +0200

    QucsApp::gotoPage: optionally handle reload of page

--- qucs/qucs.h
+++ qucs/qucs.h
@@ -95,7 +95,7 @@ public:
   bool closeAllFiles(int exceptTab = -1);
   bool closeAllLeft(int);
   bool closeAllRight(int);
-  bool gotoPage(const QString&);   // to load a document
+  bool gotoPage(const QString&, bool reloadPage = false);   // to load a document
   QucsDoc *getDoc(int No=-1);
   QucsDoc* findDoc (QString, int * Pos = 0);
   QString fileType (const QString&);
