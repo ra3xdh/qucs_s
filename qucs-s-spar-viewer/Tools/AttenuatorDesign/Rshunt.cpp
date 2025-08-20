071d20b7ac1b3646f369ba244193a1e8b5a2fcbd
andresmmera <andresmmera@protonmail.com>
2025-08-20 06:34:45 +0200

    S-par Viewer [Att Tool]: Add Zout label to Rshunt

--- qucs-s-spar-viewer/Tools/AttenuatorDesign/Rshunt.cpp
+++ qucs-s-spar-viewer/Tools/AttenuatorDesign/Rshunt.cpp
@@ -22,7 +22,7 @@ void AttenuatorDesigner::RShuntAttenuator() {
 
   ComponentInfo TermSpar1, TermSpar2;
   ComponentInfo Res1, Ground;
-  NodeInfo NIin, NIout;
+  NodeInfo NIin;
   Components.clear();
 
          // Design equations
@@ -45,20 +45,28 @@ void AttenuatorDesigner::RShuntAttenuator() {
   TermSpar1.val["Z"] = num2str(Specs.Zin, Resistance); // Effective input impedance
   Schematic.appendComponent(TermSpar1);
 
+  // Zin label
+  QString Zin_label = QString("Zin = %1 \u03A9").arg(num2str(Zin));
+  QGraphicsTextItem* label1 = new QGraphicsTextItem(Zin_label);
+  label1->setDefaultTextColor(Qt::red);
+  label1->setFont(QFont("Arial", 6, QFont::Bold));
+  label1->setPos(-20, -30);
+  Schematic.appendText(label1);
+
          // Node before shunt
-  NIin.setParams(QString("N%1").arg(++Schematic.NumberComponents[ConnectionNodes]), 100, 0);
+  NIin.setParams(QString("N%1").arg(++Schematic.NumberComponents[ConnectionNodes]), 50, 0);
   Schematic.appendNode(NIin);
 
          // Shunt resistor to ground
-  Res1.setParams(QString("R%1").arg(++Schematic.NumberComponents[Resistor]), Resistor, 0, 100, 50);
+  Res1.setParams(QString("R%1").arg(++Schematic.NumberComponents[Resistor]), Resistor, 0, 50, 50);
   Res1.val["R"] = num2str(R1, Resistance);
   Schematic.appendComponent(Res1);
 
-  Ground.setParams(QString("GND%1").arg(++Schematic.NumberComponents[GND]), GND, 0, 100, 100);
+  Ground.setParams(QString("GND%1").arg(++Schematic.NumberComponents[GND]), GND, 0, 50, 100);
   Schematic.appendComponent(Ground);
 
          // Output terminal
-  TermSpar2.setParams(QString("T%1").arg(++Schematic.NumberComponents[Term]), Term, 0, 150, 0);
+  TermSpar2.setParams(QString("T%1").arg(++Schematic.NumberComponents[Term]), Term, 0, 100, 0);
   TermSpar2.val["Z"] = num2str(Specs.Zin, Resistance); // Effective output impedance
   Schematic.appendComponent(TermSpar2);
 
@@ -74,4 +82,12 @@ void AttenuatorDesigner::RShuntAttenuator() {
 
          // Output node to terminal
   Schematic.appendWire(NIin.ID, 0, TermSpar2.ID, 0);
+
+  // Zout label
+  QString Zout_label = QString("Zout = %1 \u03A9").arg(num2str(Zout));
+  QGraphicsTextItem* label2 = new QGraphicsTextItem(Zout_label);
+  label2->setDefaultTextColor(Qt::red);
+  label2->setFont(QFont("Arial", 6, QFont::Bold));
+  label2->setPos(80, -30);
+  Schematic.appendText(label2);
 }
