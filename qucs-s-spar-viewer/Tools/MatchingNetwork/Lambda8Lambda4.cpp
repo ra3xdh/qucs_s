0dc09a4eab46f2daaab4b91eea8237e768ab2e7b
andresmmera <andresmmera@protonmail.com>
2025-08-25 04:51:54 +0200

    S-par Viewer [Matching]: Remove lambda/8 in case of a real load

--- qucs-s-spar-viewer/Tools/MatchingNetwork/Lambda8Lambda4.cpp
+++ qucs-s-spar-viewer/Tools/MatchingNetwork/Lambda8Lambda4.cpp
@@ -1,8 +1,19 @@
 /***************************************************************************
-Lambda8Lambda4.cpp
-------------------
-author : 2025: Andrés Martínez Mera
-***************************************************************************/
+                               Lambda8Lambda4.cpp
+                               ----------------------
+    copyright            :  QUCS team
+    author               :  2025: Andrés Martínez Mera
+    email                :  andresmmera@protonmail.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ ***************************************************************************/
 
 #include "Lambda8Lambda4.h"
 #include <cmath>
@@ -18,7 +29,9 @@ void Lambda8Lambda4::synthesize() {
     Schematic.appendComponent(TermSpar1);
 
 
-    // --- Design equations
+    // Design equations
+    // Reference: Inder J. Bahl. "Fundamentals of RF and microwave transistor amplifiers".
+    // John Wiley and Sons. 2009. Pages 159 - 160
     double Z0 = Specs.Zin.real();
     double RL = Specs.Zout.real();
     double XL = Specs.Zout.imag();
@@ -30,32 +43,47 @@ void Lambda8Lambda4::synthesize() {
     double Zmm = std::sqrt(RL * RL + XL * XL);
     double Zm = std::sqrt((Z0 * RL * Zmm) / (Zmm - XL));
 
+    int x_pos = 50; // x-axis position of the component
+
+    ComponentInfo TL2;
+
     // First transmission line: Zm, length λ/4
-    ComponentInfo TL1(QString("TLIN%1").arg(++Schematic.NumberComponents[TransmissionLine]), TransmissionLine, 90, 50, 0);
+    ComponentInfo TL1(QString("TLIN%1").arg(++Schematic.NumberComponents[TransmissionLine]), TransmissionLine, 90, x_pos, 0);
     TL1.val["Z0"] = num2str(Zm, Resistance);
     TL1.val["Length"] = ConvertLengthFromM("mm", l4);
     Schematic.appendComponent(TL1);
 
-    // Second transmission line: Zmm, length λ/8
-    ComponentInfo TL2(QString("TLIN%1").arg(++Schematic.NumberComponents[TransmissionLine]), TransmissionLine, 90, 100, 0);
-    TL2.val["Z0"] = num2str(Zmm, Resistance);
-    TL2.val["Length"] = ConvertLengthFromM("mm", l8);
-    Schematic.appendComponent(TL2);
+    if (XL != 0) {
+      // Second transmission line: Zmm, length λ/8
+      // It's needed as long as the load is complex to correct the imaginary part
+
+      x_pos += 50;
+      TL2.setParams(QString("TLIN%1").arg(++Schematic.NumberComponents[TransmissionLine]), TransmissionLine, 90, x_pos, 0);
+      TL2.val["Z0"] = num2str(Zmm, Resistance);
+      TL2.val["Length"] = ConvertLengthFromM("mm", l8);
+      Schematic.appendComponent(TL2);
+    }
 
     // Load
-    ComponentInfo Zload(QString("Z%1").arg(++Schematic.NumberComponents[ComplexImpedance]), ComplexImpedance, 0, 150, 50);
+
+    x_pos += 50;
+    ComponentInfo Zload(QString("Z%1").arg(++Schematic.NumberComponents[ComplexImpedance]), ComplexImpedance, 0, x_pos, 50);
     Zload.val["Z"] = num2str(Specs.Zout, Resistance);
     Schematic.appendComponent(Zload);
 
            // GND for load
     ComponentInfo GND_ZL;
-    GND_ZL.setParams(QString("GND%1").arg(++Schematic.NumberComponents[GND]), GND, 0, 150, 100);
+    GND_ZL.setParams(QString("GND%1").arg(++Schematic.NumberComponents[GND]), GND, 0, x_pos, 100);
     Schematic.appendComponent(GND_ZL);
 
     // --- Wiring ---
     Schematic.appendWire(TermSpar1.ID, 0, TL1.ID, 0);
-    Schematic.appendWire(TL1.ID, 1, TL2.ID, 0);
-    Schematic.appendWire(Zload.ID, 1, TL2.ID, 1);
+    if (XL != 0) {
+      Schematic.appendWire(TL1.ID, 1, TL2.ID, 0);
+      Schematic.appendWire(Zload.ID, 1, TL2.ID, 1);
+    } else {
+      Schematic.appendWire(Zload.ID, 1, TL1.ID, 1);
+    }
     Schematic.appendWire(Zload.ID, 0, GND_ZL.ID, 0);
 }
 
--- qucs-s-spar-viewer/Tools/MatchingNetwork/Lambda8Lambda4.h
+++ qucs-s-spar-viewer/Tools/MatchingNetwork/Lambda8Lambda4.h
@@ -1,8 +1,19 @@
 /***************************************************************************
-Lambda8Lambda4.h
-----------------
-author : 2025: Andrés Martínez Mera
-***************************************************************************/
+                               Lambda8Lambda4.h
+                               ----------------------
+    copyright            :  QUCS team
+    author               :  2025: Andrés Martínez Mera
+    email                :  andresmmera@protonmail.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ ***************************************************************************/
 
 #ifndef LAMBDA8LAMBDA4_H
 #define LAMBDA8LAMBDA4_H
