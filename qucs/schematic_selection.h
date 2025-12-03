ra3xdh/qucs_s

commit c21f8ae8a96ef06f5d4e4fa09638df252381288f

refactor(Schematic): defer node transformations to owner elements
Remove explicit node transformation handling, nodes are now
synced through their owner elements (components/wires).

Handle the special case in snapToGrid where component mvoement require
explicit syncing.

qucs/schematic_selection.h
@@ -75,7 +75,8 @@ struct SchematicSelection {
       }
     }
     for (auto* pm : markers)      pm->moveCenter(dx, dy);
-    for (auto* pn : nodes)        pn->moveCenter(dx, dy);
+    // NOTE: nodes are synced through their owner (components/wires)
+    // for (auto* pn : nodes)        pn->moveCenter(dx, dy);
 
     // Move bounds
     bounds.moveCenter(QPoint(dx, dy));

qucs/schematic_element.cpp
@@ -1699,6 +1699,7 @@ bool Schematic::elementsOnGrid(Selection selection, bool doHeal)
         any_set = internal::snapLabelToGrid(this, label) || any_set;
     });
     std::ranges::for_each(selection.markers, onGridSetter);
+    // NOTE: when using setP1/setP2, connected nodes are also moved
     std::ranges::for_each(selection.wires, [&any_set, this](Wire* w) {
         auto p1_moved = w->setP1(setOnGrid(w->P1()));
         auto p2_moved = w->setP2(setOnGrid(w->P2()));
@@ -1773,7 +1774,8 @@ bool Schematic::rotateElements(Selection selection, bool doHeal)
     std::ranges::for_each(selection.diagrams, rotator);
     std::ranges::for_each(selection.labels, rotator);
     std::ranges::for_each(selection.markers, rotator);
-    std::ranges::for_each(selection.nodes, rotator);
+    // NOTE: nodes are synced through their owner (components/wires)
+    // std::ranges::for_each(selection.nodes, rotator);
 
     if (!any_rotated) return false;
 
@@ -1825,7 +1827,8 @@ bool Schematic::mirrorXComponents(Selection selection, bool doHeal)
     std::ranges::for_each(selection.diagrams, mirrorer);
     std::ranges::for_each(selection.labels, mirrorer);
     std::ranges::for_each(selection.markers, mirrorer);
-    std::ranges::for_each(selection.nodes, mirrorer);
+    // NOTE: nodes are synced through their owner (components/wires)
+    // std::ranges::for_each(selection.nodes, mirrorer);
 
     if (!any_mirrored) return false;
 
@@ -1876,7 +1879,8 @@ bool Schematic::mirrorYComponents(Selection selection, bool doHeal)
     std::ranges::for_each(selection.diagrams, mirrorer);
     std::ranges::for_each(selection.labels, mirrorer);
     std::ranges::for_each(selection.markers, mirrorer);
-    std::ranges::for_each(selection.nodes, mirrorer);
+    // NOTE: nodes are synced through their owner (components/wires)
+    // std::ranges::for_each(selection.nodes, mirrorer);
 
     if (!any_mirrored) return false;
 

commit a040c0c66283e9558fd8b78d1fc3087ad10b3ca8

fix(MouseActions): wire stretching by dragging node
The ability to stretch a wire by dragging its node depends on moving the nodes without their owner.
In this case, we rely on the `isoNodes` container to move these non-owned nodes.

Fixes: c21f8ae8 ("refactor(Schematic): defer node transformations to owner elements")

qucs/schematic_selection.h
@@ -79,7 +79,8 @@ struct SchematicSelection {
     }
     for (auto* pm : markers)      pm->moveCenter(dx, dy);
     // NOTE: nodes are synced through their owner (components/wires)
-    // for (auto* pn : nodes)        pn->moveCenter(dx, dy);
+    // Special case: if we have isolated nodes, we move those
+    for (auto* pn : isoNodes)     pn->moveCenter(dx, dy);
 
     // Move bounds
     bounds.moveCenter(QPoint(dx, dy));