ra3xdh/qucs_s

commit 09923a89c77512aac93cade97de7f72c8fff535a

plot noise contributors

qucs/extsimkernels/ngspice.cpp
@@ -272,7 +272,7 @@ void Ngspice::createNetlist(
             } else {  // Set Noise1 plot to output noise spectrum
                 spiceNetlist.append("setplot noise1\n");
             }
-            nods = "inoise_spectrum onoise_spectrum";
+            nods = "noise1.all";
         } else if ( sim_typ == ".PZ" ) {
             pzSims++;
             spiceNetlist.append(pc->getSpiceNetlist());
@@ -325,7 +325,24 @@ void Ngspice::createNetlist(
             QString out = "spice4qucs." + sim_name + ".ngspice.dc.print";
             spiceNetlist.append(QStringLiteral("print %1 > %2\n").arg(nods).arg(out));
             outputs.append(out);
-        } else if ( (sim_typ != ".PZ") && (sim_typ != ".SENS") && (sim_typ != ".SENS_AC") ) {
+        } 
+        else if (sim_typ == ".NOISE") {
+            nods = nods.simplified();
+            if ( !nods.isEmpty() ) {
+                QString basenam = "spice4qucs";
+                QString filename;
+                if ( hasParSWP && hasDblSWP )
+                    filename = QStringLiteral("%1.%2._swp_swp.raw").arg(basenam).arg(sim_name);
+                else if ( hasParSWP )
+                    filename = QStringLiteral("%1.%2._swp.raw").arg(basenam).arg(sim_name);
+                else
+                    filename = QStringLiteral("%1.%2.raw").arg(basenam).arg(sim_name);
+                filename.replace(' ', '_'); // Ngspice cannot understand spaces in filename
+                spiceNetlist.append(QStringLiteral("write %1 %2\n").arg(filename).arg(nods));
+                outputs.append(filename);
+            }
+        }
+        else if ( (sim_typ != ".PZ") && (sim_typ != ".SENS") && (sim_typ != ".SENS_AC") ) {
             nods = nods.simplified();
             if ( !nods.isEmpty() ) {
                 QString basenam = "spice4qucs";

commit cadc9fd29086a1e8d5f8ecd73f8bcffc50d79e86

fix(ngspice/noise): save nodes relative to plot number
This change uses the relative plot to save the correct
nodes. We use 'setplot noiseX' and then use the relative
wildcard save 'all' to get the equivalent of 'noiseX.all'.

Previosuly, when using the fixed value of
'noise1.all' it would end up using the first result for every plot.
Making it seems like there was one singular plot in the case of sweeps.

Fixes: 09923a89 ("plot noise contributors")

qucs/extsimkernels/ngspice.cpp
@@ -272,7 +272,9 @@ void Ngspice::createNetlist(
             } else {  // Set Noise1 plot to output noise spectrum
                 spiceNetlist.append("setplot noise1\n");
             }
-            nods = "noise1.all";
+            // NOTE: if we set 'setplot noiseX', and we use 'all'
+            // it will be the equivalent of noiseX.all
+            nods = "all";
         } else if ( sim_typ == ".PZ" ) {
             pzSims++;
             spiceNetlist.append(pc->getSpiceNetlist());