ra3xdh/qucs_s

commit c76be4b5a15efcb624b16a1246233021b7df69a0

fix(Mouseactions): return to select mode after keyboard-move
From a user perspective one expects to go back to the "normal" state after a keyboard move.
This fixes this, by doing `slotEscape()` after a succesful operation.

qucs/mouseactions.cpp
@@ -1442,14 +1442,24 @@ void MouseActions::MReleaseMoving(Schematic *Doc, QMouseEvent* event)
     Doc->viewport()->update();
     Doc->releaseKeyboard(); // allow keyboard inputs again
 
+    // Clear move action
     QucsMain->MouseMoveAction = nullptr;
-    QucsMain->MousePressAction = &MouseActions::MPressSelect;
-    QucsMain->MouseReleaseAction = &MouseActions::MReleaseSelect;
-    QucsMain->MouseDoubleClickAction = &MouseActions::MDoubleClickSelect;
+
     QucsMain->editRotate->setChecked(false);
     QucsMain->editRotate->blockSignals(false);
     QucsMain->insLabel->blockSignals(false);
     QucsMain->setMarker->blockSignals(false);
+
+    // if we used drag 'n' drop, we might already be in select mode
+    // -> Manually re-enter select mode
+    if (QucsMain->activeAction == QucsMain->select) {
+        QucsMain->MousePressAction = &MouseActions::MPressSelect;
+        QucsMain->MouseReleaseAction = &MouseActions::MReleaseSelect;
+        QucsMain->MouseDoubleClickAction = &MouseActions::MDoubleClickSelect;
+        return;
+    }
+    // If we're in keyboard based movement, go back to select mode by slotting
+    QucsMain->slotEscape();
 }
 // -----------------------------------------------------------
 // Is called after move-free (move with disconnection) operation
@@ -1472,11 +1482,9 @@ void MouseActions::MReleaseMoveFree(Schematic *Doc, QMouseEvent *Event)
     QucsMain->MouseReleaseAction = nullptr;
     QucsMain->MouseDoubleClickAction = nullptr;
 
-    QucsMain->editMove->blockSignals(true);
-    QucsMain->editMove->setChecked(false);
-    QucsMain->editMove->blockSignals(false);
-
-    QucsMain->slotSelect(false);
+    // Go back to select mode
+    // NOTE: This turns off @activeAction
+    QucsMain->slotEscape();
 }
 
 // -----------------------------------------------------------

qucs/qucs_actions.cpp
@@ -238,15 +238,18 @@ void QucsApp::slotEditDelete(bool on)
 void QucsApp::slotEditStretch(bool on)
 {
   Schematic* Doc = dynamic_cast<Schematic*>(DocumentTab->currentWidget());
-  if(!on || Doc->currentSelection().isEmpty()) {
-    // Can't perform action without selection
+  if (!on || Doc->currentSelection().isEmpty()) {
+    // if we were already on, or selection is empty
+    // cancel action and return to select mode
     editStretch->blockSignals(true);
     editStretch->setChecked(false);
     editStretch->blockSignals(false);
+
+    activeAction = nullptr;
+
+    slotEscape();
     return;
   }
-  // Add undo entry in case we cancel move action
-  Doc->setChanged(true, true);
 
   performToggleAction(on, editStretch, nullptr,
     &MouseActions::MMoveMoving, nullptr);
@@ -257,11 +260,16 @@ void QucsApp::slotEditStretch(bool on)
 void QucsApp::slotEditMove(bool on)
 {
   Schematic* Doc = dynamic_cast<Schematic*>(DocumentTab->currentWidget());
-  if(!on || Doc->currentSelection().isEmpty()) {
-    // Can't perform action without selection
+  if (!on || Doc->currentSelection().isEmpty()) {
+    // if we were already on, or selection is emtpy
+    // cancel action and return to select mode
     editMove->blockSignals(true);
     editMove->setChecked(false);
     editMove->blockSignals(false);
+
+    activeAction = nullptr;
+
+    slotEscape();
     return;
   }
 

commit 3dc1aa2d5f0d708403b5691eb5ece2170292755a

fix(mouseactions): properly deactivate action when releasing keyboard-move
This explicitly deactivates the current action in the case we are currently
in a keyboard-move (i.e. either stretch or isolated move).
Previously when this wasn't deactivated, one could enter a state
where one wasn't in a _proper_ selection mode, making it seem like the GUI
wasn't interactive anymore.

fixes: c76be4b5 ("fix(Mouseactions): return to select mode after keyboard-move")

qucs/mouseactions.cpp
@@ -1450,16 +1450,20 @@ void MouseActions::MReleaseMoving(Schematic *Doc, QMouseEvent* event)
     QucsMain->insLabel->blockSignals(false);
     QucsMain->setMarker->blockSignals(false);
 
-    // if we used drag 'n' drop, we might already be in select mode
-    // -> Manually re-enter select mode
-    if (QucsMain->activeAction == QucsMain->select) {
+    QAction* action = QucsMain->activeAction;
+    if (action == nullptr || action == QucsMain->select) {
+        // if we used drag 'n' drop, we might already be in select mode
+        // -> Manually re-enter select mode
         QucsMain->MousePressAction = &MouseActions::MPressSelect;
         QucsMain->MouseReleaseAction = &MouseActions::MReleaseSelect;
         QucsMain->MouseDoubleClickAction = &MouseActions::MDoubleClickSelect;
-        return;
+    } else {
+        // If we're in keyboard based movement, cleanup and go back to select mode by slotting
+        action->blockSignals(true);
+        action->setChecked(false);
+        action->blockSignals(false);
+        QucsMain->slotEscape();
     }
-    // If we're in keyboard based movement, go back to select mode by slotting
-    QucsMain->slotEscape();
 }
 // -----------------------------------------------------------
 // Is called after move-free (move with disconnection) operation
@@ -1478,12 +1482,13 @@ void MouseActions::MReleaseMoveFree(Schematic *Doc, QMouseEvent *Event)
 
     // Reset everything and go back to select mode
     QucsMain->MouseMoveAction = nullptr;
-    QucsMain->MousePressAction = nullptr;
-    QucsMain->MouseReleaseAction = nullptr;
-    QucsMain->MouseDoubleClickAction = nullptr;
 
-    // Go back to select mode
-    // NOTE: This turns off @activeAction
+    // cleanup current action if any
+    if (QucsMain->activeAction != nullptr) {
+        QucsMain->activeAction->blockSignals(true);
+        QucsMain->activeAction->setChecked(false);
+        QucsMain->activeAction->blockSignals(false);
+    }
     QucsMain->slotEscape();
 }
 

qucs/qucs_actions.cpp
@@ -242,15 +242,12 @@ void QucsApp::slotEditDelete(bool on) {
 // Is called if "Strech (move w/wiring)"-Button is pressed.
 void QucsApp::slotEditStretch(bool on) {
   Schematic *Doc = dynamic_cast<Schematic *>(DocumentTab->currentWidget());
-  if (!on || Doc->currentSelection().isEmpty()) {
-    // if we were already on, or selection is empty
-    // cancel action and return to select mode
+  // if we were already on and  selection is empty
+  // cancel action and return to select mode
+  if (on && Doc->currentSelection().isEmpty()) {
     editStretch->blockSignals(true);
     editStretch->setChecked(false);
     editStretch->blockSignals(false);
-
-    activeAction = nullptr;
-
     slotEscape();
     return;
   }
@@ -263,15 +260,12 @@ void QucsApp::slotEditStretch(bool on) {
 // Is called if "Move (w/o wiring)"-Button is pressed.
 void QucsApp::slotEditMove(bool on) {
   Schematic *Doc = dynamic_cast<Schematic *>(DocumentTab->currentWidget());
-  if (!on || Doc->currentSelection().isEmpty()) {
-    // if we were already on, or selection is emtpy
-    // cancel action and return to select mode
+  // if we were already on and selection is empty
+  // cancel action and return to select mode
+  if (on && Doc->currentSelection().isEmpty()) {
     editMove->blockSignals(true);
     editMove->setChecked(false);
     editMove->blockSignals(false);
-
-    activeAction = nullptr;
-
     slotEscape();
     return;
   }